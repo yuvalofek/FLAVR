6d2363ab6d3abbb1f72311e3500e27370d80a249
Yuval Ofek <35583988+yuvalofek@users.noreply.github.com>
2021-08-04 16:20:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -57,9 +57,10 @@ elif args.dataset == "gopro":
     train_loader = get_loader(args.data_root, args.batch_size, shuffle=True, num_workers=args.num_workers, test_mode=False, interFrames=args.n_outputs, n_inputs=args.nbr_frame)
     test_loader = get_loader(args.data_root, args.batch_size, shuffle=False, num_workers=args.num_workers, test_mode=True, interFrames=args.n_outputs, n_inputs=args.nbr_frame)
 elif args.dataset == "satellite":
-    from dataset.Satellite import get_loader
-    train_loader = get_loader(args.data_root, args.batch_size, shuffle=True, num_workers=args.num_workers, is_training=True, inter_frames=args.n_outputs, n_inputs=args.nbr_frame)
-    test_loader = get_loader(args.data_root, args.batch_size, shuffle=False, num_workers=args.num_workers, is_training=False, inter_frames=args.n_outputs, n_inputs=args.nbr_frame)
+    from dataset.Satellite import get_loader, get_train_test
+    tr_paths, test_paths = get_train_test(args.data_root, random_state=214)
+    train_loader = get_loader(tr_paths, args.batch_size, shuffle=True, num_workers=args.num_workers, is_training=True, inter_frames=args.n_outputs, n_inputs=args.nbr_frame)
+    test_loader = get_loader(test_paths, args.batch_size, shuffle=False, num_workers=args.num_workers, is_training=False, inter_frames=args.n_outputs, n_inputs=args.nbr_frame)
 else:
     raise NotImplementedError
 
